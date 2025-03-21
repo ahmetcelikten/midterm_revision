741be3ce962730730721c54c92f65773b560c88e
Ahmet <ahmetcelikten@hotmail.com.tr>
2025-03-21 23:06:39 +0300

    Add files via upload

---
+++ revision_volumeControl/revision_volumeControl/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace revision_volumeControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.btnMute = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.LargeChange = 10;
+            this.trackBarVolume.Location = new System.Drawing.Point(257, 138);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(104, 45);
+            this.trackBarVolume.SmallChange = 5;
+            this.trackBarVolume.TabIndex = 0;
+            this.trackBarVolume.TickFrequency = 10;
+            this.trackBarVolume.Value = 50;
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblVolume.Location = new System.Drawing.Point(379, 160);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(103, 20);
+            this.lblVolume.TabIndex = 1;
+            this.lblVolume.Text = "Volume: %50";
+            // 
+            // btnMute
+            // 
+            this.btnMute.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMute.Location = new System.Drawing.Point(176, 160);
+            this.btnMute.Name = "btnMute";
+            this.btnMute.Size = new System.Drawing.Size(75, 23);
+            this.btnMute.TabIndex = 2;
+            this.btnMute.Text = "MUTE";
+            this.btnMute.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPlay.Location = new System.Drawing.Point(12, 160);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPause.Location = new System.Drawing.Point(93, 160);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "PAUSE";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(322, 55);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTime.Location = new System.Drawing.Point(135, 79);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(226, 39);
+            this.lblTime.TabIndex = 6;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 195);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnMute);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.trackBarVolume);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarVolume;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Button btnMute;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ revision_volumeControl/revision_volumeControl/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace revision_volumeControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            trackBarVolume.Minimum = 0;
+            trackBarVolume.Maximum = 100;
+            trackBarVolume.Value = 50;
+            axWindowsMediaPlayer1.settings.volume = trackBarVolume.Value;
+            axWindowsMediaPlayer1.Visible = false;
+            lblVolume.Text = $"Volume: {trackBarVolume.Value}%";
+
+            trackBarVolume.Scroll += (s, e) => AdjustVolume();
+            btnMute.Click += (s, e) => MuteMusic();
+            btnPlay.Click += (s, e) => PlayMusic();
+            btnPause.Click += (s, e) => PauseMusic();
+            timer1.Tick += (s, e) => UpdateTime();
+        }
+
+        private void PlayMusic()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 Files|*.mp3";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = openFileDialog.FileName;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                timer1.Start(); 
+            }
+        }
+
+        private void PauseMusic()
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void MuteMusic()
+        {
+            axWindowsMediaPlayer1.settings.volume = 0;
+            trackBarVolume.Value = 0;
+        }
+        private void AdjustVolume()
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBarVolume.Value;
+            lblVolume.Text = $"Volume: {trackBarVolume.Value}%";
+        }
+        private void UpdateTime()
+        {
+            if (axWindowsMediaPlayer1.currentMedia != null)
+            {
+                double currentTime = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+                double duration = axWindowsMediaPlayer1.currentMedia.duration;
+
+                lblTime.Text = $"{FormatTime(currentTime)} / {FormatTime(duration)}";
+            }
+        }
+
+        private string FormatTime(double seconds)
+        {
+            int min = (int)(seconds / 60);
+            int sec = (int)(seconds % 60);
+            return $"{min:D2}:{sec:D2}";
+        }
+    }
+}
