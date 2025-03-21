6176cf13730ed4977e7918d9062bfa0d93f77015
Ahmet <ahmetcelikten@hotmail.com.tr>
2025-03-21 22:44:25 +0300

    Add files via upload

---
+++ revision_Hscgorllbar/revision_Hscgorllbar/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace revision_Hscgorllbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hScrollBarRed = new System.Windows.Forms.HScrollBar();
+            this.hScrollBarGreen = new System.Windows.Forms.HScrollBar();
+            this.hScrollBarBlue = new System.Windows.Forms.HScrollBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hScrollBarRed
+            // 
+            this.hScrollBarRed.LargeChange = 1;
+            this.hScrollBarRed.Location = new System.Drawing.Point(18, 31);
+            this.hScrollBarRed.Maximum = 225;
+            this.hScrollBarRed.Name = "hScrollBarRed";
+            this.hScrollBarRed.Size = new System.Drawing.Size(231, 125);
+            this.hScrollBarRed.TabIndex = 0;
+            this.hScrollBarRed.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBarGreen
+            // 
+            this.hScrollBarGreen.LargeChange = 1;
+            this.hScrollBarGreen.Location = new System.Drawing.Point(280, 31);
+            this.hScrollBarGreen.Maximum = 225;
+            this.hScrollBarGreen.Name = "hScrollBarGreen";
+            this.hScrollBarGreen.Size = new System.Drawing.Size(241, 125);
+            this.hScrollBarGreen.TabIndex = 1;
+            // 
+            // hScrollBarBlue
+            // 
+            this.hScrollBarBlue.LargeChange = 1;
+            this.hScrollBarBlue.Location = new System.Drawing.Point(554, 31);
+            this.hScrollBarBlue.Maximum = 225;
+            this.hScrollBarBlue.Name = "hScrollBarBlue";
+            this.hScrollBarBlue.Size = new System.Drawing.Size(237, 130);
+            this.hScrollBarBlue.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(345, 418);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // lblColor
+            // 
+            this.lblColor.BackColor = System.Drawing.SystemColors.InfoText;
+            this.lblColor.Location = new System.Drawing.Point(95, 197);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(633, 192);
+            this.lblColor.TabIndex = 4;
+            this.lblColor.Text = "label1";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnReset.Location = new System.Drawing.Point(18, 418);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.hScrollBarBlue);
+            this.Controls.Add(this.hScrollBarGreen);
+            this.Controls.Add(this.hScrollBarRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBarRed;
+        private System.Windows.Forms.HScrollBar hScrollBarGreen;
+        private System.Windows.Forms.HScrollBar hScrollBarBlue;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ revision_Hscgorllbar/revision_Hscgorllbar/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace revision_Hscgorllbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateColor();
+
+            hScrollBarRed.Scroll += (s, e) => UpdateColor();
+            hScrollBarGreen.Scroll += (s, e) => UpdateColor();
+            hScrollBarBlue.Scroll += (s, e) => UpdateColor();
+            numericUpDown1.ValueChanged += (s, e) => UpdateColor();
+            btnReset.Click += (s, e) => ResetColor();
+        }
+
+        private void UpdateColor()
+        {
+            int red = hScrollBarRed.Value;
+            int green = hScrollBarGreen.Value;
+            int blue = hScrollBarBlue.Value;
+            int alpha = decimal.ToInt32(numericUpDown1.Value * 2.55m);
+
+            lblColor.BackColor = Color.FromArgb(alpha, red, green, blue);
+        }
+
+        private void ResetColor()
+        {
+            hScrollBarRed.Value = 0;
+            hScrollBarGreen.Value = 0;
+            hScrollBarBlue.Value = 0;
+            numericUpDown1.Value = 100;
+            UpdateColor();
+        }
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
